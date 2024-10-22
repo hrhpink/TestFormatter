dc690a4643197bc25ae5210d3039b54122b82fc0
Natalie O'Leary <hrhpink@gmail.com>
2024-10-21 22:57:18 -0400

    - Updated landing page appearance - Changed button event for "Create a new exam" to open dialog for file name - Added dialog for writing file name for new exam - When "submit" on the file name dialog is clicked, navigate to main formatting page - Added data binding to connect textbox input to code behind variable - Changed button event for "Load previous exam" to open a file dialog for .csv file attachment - Removed SamplePage and SampleWindow since they are no longer needed

--- TestFormatter/Pages/LandingPage.xaml.cs
+++ TestFormatter/Pages/LandingPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -27,14 +28,32 @@ namespace TestFormatter.Pages
 
         private void CreateExam_Click(object sender, RoutedEventArgs e)
         {
-            //this.NavigationService.Navigate(new Uri("/Pages/SamplePage.xaml", UriKind.Relative));
-            Window win2 = new Windows.SampleWindow();
+
+            //Ask user to enter file name
+            Window win2 = new Windows.FileNameInput();
             win2.ShowDialog();
+       
+            //Navigate to formatter
+            this.NavigationService.Navigate(new Uri("/Pages/FormatterPage.xaml", UriKind.Relative));
         }
 
         private void LoadExam_Click(object sender, RoutedEventArgs e)
         {
-            this.NavigationService.Navigate(new Uri("/Pages/SamplePage.xaml", UriKind.Relative));
+            var dialog = new Microsoft.Win32.OpenFileDialog();
+            dialog.DefaultExt = ".csv"; // Default extension
+            dialog.Filter = "Text documents (.csv)|*.csv"; // Filter files by extension
+
+            // Show open file dialog box
+           if (dialog.ShowDialog() == true)
+            {
+                string filepath = dialog.FileName;
+                //TODO: store name in a place that program can access at runtime so that it can display it
+                var fileStream = dialog.OpenFile();
+                using (StreamReader sr = new StreamReader(fileStream)) { 
+                //TODO: parse csv file
+                }
+
+            }
         }
     }
 }
---
+++ TestFormatter/Windows/FileNameInput.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TestFormatter.Windows
+{
+    /// <summary>
+    /// Interaction logic for FileNameInput.xaml
+    /// </summary>
+    public partial class FileNameInput : Window
+    {
+        public FileNameInput()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        private string _fileName;
+        public string fileName
+        {
+            get { return _fileName; }
+            set { 
+                _fileName = value;
+            }
+            //TODO: store file name somewhere.
+        }
+
+        private void ConfirmName_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
